9158be0786178a7d95a717f189b4332923f8d3fc
lakshitajadon2c <90979337+lakshitajadon2c@users.noreply.github.com>
2021-09-25 19:16:36 +0530

    create prime.c
    the number is prime or not

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int numberisprimeornot(int);
+int main()
+{
+  int x,i;
+  printf("enter a number");
+  scanf("%d",&x);
+for(i=2;i<=x-1;i++)
+  if(x/1==0)
+  break;
+  
+if(i==x)
+   printf(%d is a prime number",x);
+else
+   printf("%d is not a prime number",x);
+}
+  
